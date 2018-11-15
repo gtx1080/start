4976496534651388e5d596cb9089415adb0b2f43
gtx1080 <2877359418@qq.com>
2018-11-15 10:55:43 +0800

    Add files via upload

--- Homework/shoot.py
+++ Homework/shoot.py
@@ -14,11 +14,22 @@ class stack():
         return a
     def read(self):
         return self.stack1
-a=1179087#十进制
+str1='22A'
+start=1
+jz=16
+res0=stack()
+a=0
+num=['0','1','2','3','4','5','6','7','8','9']
+for i in range(len(str1)-1,-1,-1):
+    if (str1[i]) in num:
+        a=a+int(str1[i])*start
+    else:
+        a=a+(ord(str1[i])-55)*start
+    start=start*jz
 res=stack()
+print(a)
 b=[]
-n=20
-lis=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+n=16
 while a:
     res.push(a%n)
     a=a//n
